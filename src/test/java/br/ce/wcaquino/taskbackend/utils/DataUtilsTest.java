3cee341c8bca88482341b0aa6b4b47bdfb156265
bruno <brunohgarcia@outlook.com>
2025-08-26 00:57:34 +0200

    including new unit tests

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DataUtilsTest.java
@@ -0,0 +1,35 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+
+public class DataUtilsTest {
+
+
+
+
+    @Test
+    public void dataFuturaDeveRetornarPositivo(){
+        LocalDate date = LocalDate.of(2026, 1, 1);
+        DateUtils dateUtils = new DateUtils();
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void dataAtualDeveRetornarPositivo(){
+        LocalDate date = LocalDate.now();
+        DateUtils dateUtils = new DateUtils();
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void dataPassadaDeveRetornarNegativo(){
+        LocalDate date = LocalDate.of(2025, 4, 4);
+        DateUtils dateUtils = new DateUtils();
+        Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+    }
+
+}
